05c19c5948ad1ce139a1e6a1d8c8d31f71fa90cc
lol bong <wilfredpascua.1990@gmail.com>
2024-01-03 22:17:36 +0800

    2nd Upload

--- src/Pages/Pricing.jsx
+++ src/Pages/Pricing.jsx
@@ -18,7 +18,7 @@ export default function Pricing() {
             dicta iusto doloremque unde!
           </p>
         </div>
-        <img src="img-2.jpg" alt="view of a large city" className="image" />
+        <img src="../img-2.jpg" alt="view of a large city" className="image" />
       </section>
     </main>
   );
