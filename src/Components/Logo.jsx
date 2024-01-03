56020c9699814fc13675884f95f6a381a4bc7c26
lol bong <wilfredpascua.1990@gmail.com>
2024-01-03 22:14:36 +0800

    2nd Upload

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,6 +1,6 @@
 import styles from "../Components/Logo.module.css";
 function Logo() {
-  return <img src="/rocket.png/" alt="logo of mine" className={styles.logo} />;
+  return <img src="rocket.png" alt="logo of mine" className={styles.logo} />;
 }
 
 export default Logo;
