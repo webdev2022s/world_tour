e4df75dd3193cf7a8b50323f9efe6b1011c76460
lol bong <wilfredpascua.1990@gmail.com>
2024-01-04 14:27:30 +0800

    Updates

--- assets/LogoAppUpper-2dce75ef.js
+++
@@ -1,1 +0,0 @@
-import{j as o,N as s}from"./index-8e4535b2.js";const r="_logo_1pk9e_1",t={logo:r};function l(){return o.jsx(s,{to:"/world_tour/",children:o.jsxs("div",{className:t.logo,children:[o.jsx("img",{src:"rocket.png",alt:"upper Logo"}),o.jsx("span",{children:"My Travel List"})]})})}export{l as L};
---
+++ assets/PageNotFound-36f740f6.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-c6c8f47c.js";function e(){return n.jsx(n.Fragment,{children:n.jsx("h1",{children:"Page not found 😭"})})}export{e as default};
